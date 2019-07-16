4f5d865dcaedb2a41ae5d7385c13eeae1a4bebfe
Trai Lynne <trailynne@gmail.com>
2019-07-16 15:37:35 -0400

    Updates

---
+++ precache-manifest.ae312760b69a335d7b0508473b018908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543a6f1544e03835abeeb03b08573efa",
+    "url": "/Conways-Life/index.html"
+  },
+  {
+    "revision": "d7c17e8fe1167df29717",
+    "url": "/Conways-Life/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/Conways-Life/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d7c17e8fe1167df29717",
+    "url": "/Conways-Life/static/js/main.d28cddba.chunk.js"
+  },
+  {
+    "revision": "f94638f27adfa8c548ad",
+    "url": "/Conways-Life/static/js/runtime~main.5602915e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Conways-Life/static/media/logo.5d5d9eef.svg"
+  }
+]);
