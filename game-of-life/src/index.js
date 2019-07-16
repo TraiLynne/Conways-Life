d4bf1d66868b305cec8aae2588ac52b301eacfad
Trai Lynne <trailynne@gmail.com>
2019-07-16 16:16:02 -0400

    Define Store Pass reducer to createStore method provided by redux

--- game-of-life/src/index.js
+++ game-of-life/src/index.js
@@ -14,6 +14,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 // store definition
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
