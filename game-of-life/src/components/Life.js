2b10b3089d203cbf91301c313510402201d8b055
Trai Lynne <trailynne@gmail.com>
2019-07-17 19:32:26 -0400

    Simplifying Components Life component for game play logic LifeCanvas for displaying

---
+++ game-of-life/src/components/Life.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// Game Play Logic is in here
+export default class Life extends Component {
+    constructor(){
+
+    }
+
+    render(){
+        return;
+    }
+}
---
+++ game-of-life/src/components/LifeCanvas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LifeCanvas extends Component {
+    constructor(){
+
+    }
+
+    render(){
+        return <canvas ref="canvas"/>;
+    }
+}
